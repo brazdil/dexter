f422a1243b5ef666024fa5d6f11823ffb31cf394
David Brazdil <david.brazdil@gmail.com>
2013-07-21 19:44:47 +0100

    Integrated ProgressCircleView into the project

--- android-gui/src/main/java/uk/ac/cam/db538/dexter/android/InstrumentActivity.java
+++ android-gui/src/main/java/uk/ac/cam/db538/dexter/android/InstrumentActivity.java
@@ -5,9 +5,11 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.widget.TextView;
 
+import org.apache.commons.io.FileUtils;
 import org.jf.dexlib.DexFile;
 import org.jf.dexlib.DexFileFromMemory;
 
@@ -15,6 +17,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
+import uk.ac.cam.db538.dexter.apk.Apk;
 import uk.ac.cam.db538.dexter.dex.AuxiliaryDex;
 import uk.ac.cam.db538.dexter.dex.Dex;
 import uk.ac.cam.db538.dexter.dex.type.ClassRenamer;
@@ -23,23 +26,37 @@ import uk.ac.cam.db538.dexter.utils.Pair;
 
 public class InstrumentActivity extends Activity {
 
+    private Typeface ttfRobotoLight;
+    private Typeface ttfRobotoThin;
+
     private ActivityManager activityManager;
     private PackageManager packageManager;
 
     private PackageInfo packageInfo;
     private File packageFile;
 
-    private EditText textTerminal;
+    private TextView textStatus;
+    private ProgressCircleView progressCircle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrument);
 
+        this.getActionBar().hide();
+
         activityManager = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
         packageManager = this.getPackageManager();
 
-        textTerminal = (EditText) findViewById(R.id.terminal);
+        textStatus = (TextView) findViewById(R.id.textStatus);
+        progressCircle = (ProgressCircleView) findViewById(R.id.progressCircle);
+
+        // load fonts
+        ttfRobotoLight = Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf");
+        ttfRobotoThin = Typeface.createFromAsset(getAssets(), "Roboto-Thin.ttf");
+
+        textStatus.setTypeface(ttfRobotoLight);
+        progressCircle.setTypeface(ttfRobotoThin);
 
         if (savedInstanceState == null) {
             // initialize
@@ -74,56 +91,62 @@ public class InstrumentActivity extends Activity {
         @Override
         public void run() {
             try {
+                File localFile = new File(InstrumentActivity.this.getFilesDir(), "app.apk");
                 DexterApplication thisApp = (DexterApplication) getApplication();
 
-                terminalMessage("Analyzing operating system");
+                setStatus("Loading files");
+
+                /*
+                 * We could load the DexFiles while framework is being loade, but that might
+                 * use too much memory. This way, only one .dex file is being loaded at
+                 * any point of time.
+                 */
+
                 thisApp.waitForHierarchy();
-                terminalDone();
 
-                terminalMessage("Loading application");
-                DexFile fileApp = new DexFile(packageFile);
+                FileUtils.copyFile(packageFile, localFile);
+                DexFile fileApp = new DexFile(localFile);
                 DexFile fileAux = new DexFileFromMemory(thisApp.getAssets().open("dexter_aux.dex"));
-                terminalDone();
 
-                terminalMessage("Building runtime hierarchy");
                 Pair<RuntimeHierarchy, ClassRenamer> buildData = thisApp.getRuntimeHierarchy(fileApp, fileAux);
                 RuntimeHierarchy hierarchy = buildData.getValA();
                 ClassRenamer renamerAux = buildData.getValB();
-                terminalDone();
-    
-                terminalMessage("Parsing application");
-                AuxiliaryDex dexAux = new AuxiliaryDex(fileAux, hierarchy, renamerAux); 
-                Dex dexApp = new Dex(fileApp, hierarchy, dexAux);
-                terminalDone();
+
+                setStatus("Analyzing");
+                Dex dexApp = new Dex(fileApp, hierarchy, new AuxiliaryDex(fileAux, hierarchy, renamerAux));
+
+                buildData = null;
+                fileApp = null;
+                fileAux = null;
+                renamerAux = null;
+                System.gc();
+
+                setStatus("Modifying");
 
 //                terminalMessage("Instrumenting application");
 //                dex.instrument(false);
 //                terminalDone();
 
-                terminalMessage("Compiling application");
-                dexApp.writeToFile();
-                terminalDone();
+                setStatus("Assembling");
+                byte[] fileApp_New = dexApp.writeToFile();
+
+                setStatus("Signing");
+                // Apk.produceAPK(localFile, localFile, null, fileApp_New);
+
+                setStatus("DONE");
 
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
         }
 
-        private void appendToTerminal(final String text) {
+        private void setStatus(final String status) {
             InstrumentActivity.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    textTerminal.append(text);
+                    textStatus.setText(status + "...");
                 }
             });
-        }
-
-        private void terminalMessage(String msg) {
-            appendToTerminal(msg + "...");
-        }
-
-        private void terminalDone() {
-            appendToTerminal(" DONE\n");
         }
     };
 
---
+++ android-gui/src/main/java/uk/ac/cam/db538/dexter/android/ProgressCircleView.java
@@ -0,0 +1,212 @@
+package uk.ac.cam.db538.dexter.android;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by db538 on 7/19/13.
+ */
+public class ProgressCircleView extends View {
+
+    private int value = 42;
+
+    private int colorText = 0xff000000;
+    private int colorCircle = 0xff000000; // ff2980b9, ff3498db;
+    private int colorBackground = 0xffffffff;
+
+    private Paint paintText;
+    private Paint paintOuterCircle;
+    private Paint paintInnerCircle;
+
+    private float factorOuter = 0.7f;
+    private float factorInner = 0.8f;
+    private float factorText = 0.8f;
+    private float factorPercent = 0.6f;
+
+    private float radiusOuter;
+    private float radiusInner;
+    private float radiusText;
+    private float sizeText;
+
+    private PointF posCenter;
+    private RectF rectOuterCircle;
+    private RectF rectOuterCircle_extended;
+    private RectF rectInnerCircle;
+    private PointF posText_Value;
+    private PointF posText_Percent;
+
+    public ProgressCircleView(Context context) {
+        super(context);
+
+        init();
+    }
+
+    private int parseColor(TypedArray a, int attr, int def) {
+        try {
+            return a.getColor(attr, def);
+        } catch (NumberFormatException ex) {
+            return a.getResources().getColor(a.getResourceId(attr, android.R.color.black));
+        }
+    }
+
+    public ProgressCircleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.ProgressCircle, 0, 0);
+        try {
+            colorCircle = parseColor(a, R.styleable.ProgressCircle_circleColor, colorCircle);
+            colorText = parseColor(a, R.styleable.ProgressCircle_textColor, colorText);
+            value = a.getInteger(R.styleable.ProgressCircle_value, value);
+        } finally {
+            a.recycle();
+        }
+
+        init();
+    }
+
+    private void init() {
+        // get background color (if exists)
+        Drawable bg = this.getBackground();
+        if (bg instanceof ColorDrawable)
+            colorBackground = ((ColorDrawable) bg).getColor();
+
+        paintText = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintText.setColor(colorText);
+
+        paintOuterCircle = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintOuterCircle.setColor(colorCircle);
+        paintOuterCircle.setStyle(Paint.Style.FILL);
+
+        paintInnerCircle = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paintInnerCircle.setColor(colorBackground);
+        paintInnerCircle.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+        computeTextPosition();
+
+        this.invalidate();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        radiusOuter = (Math.min(w, h) >> 1) * factorOuter;
+        radiusInner = radiusOuter * factorInner;
+        radiusText = radiusInner * factorText;
+
+        float posxCenter = w >> 1;
+        float posyCenter = h >> 1;
+        posCenter = new PointF(posxCenter, posyCenter);
+
+        rectOuterCircle = new RectF(
+                posxCenter - radiusOuter,
+                posyCenter - radiusOuter,
+                posxCenter + radiusOuter,
+                posyCenter + radiusOuter);
+        rectOuterCircle_extended = new RectF(
+                rectOuterCircle.left - 1,
+                rectOuterCircle.top - 1,
+                rectOuterCircle.right + 1,
+                rectOuterCircle.bottom + 1);
+        rectInnerCircle = new RectF(
+                posxCenter - radiusInner,
+                posyCenter - radiusInner,
+                posxCenter + radiusInner,
+                posyCenter + radiusInner);
+
+        // compute the desired font height
+        float const_height = 1000.0f;
+        float width_over_height = computeValueTextWidth(const_height, 100) / const_height;
+        sizeText = (float) Math.sqrt(4 * radiusText * radiusText / (1 + width_over_height * width_over_height));
+
+        computeTextPosition();
+    }
+
+    public void setTypeface(Typeface ttf) {
+        paintText.setTypeface(ttf);
+    }
+
+    private Rect getStringRect(String str) {
+        Rect rect = new Rect();
+        paintText.getTextBounds(str, 0, str.length(), rect);
+        return rect;
+    }
+
+    private float computeValueTextWidth() {
+        return computeValueTextWidth(sizeText, value);
+    }
+
+    private float computeValueTextWidth(float height, int value) {
+        paintText.setTextSize(height);
+        float widthNumber = getStringRect(Integer.toString(value)).right;
+        paintText.setTextSize(height * factorPercent);
+        float widthPercent = getStringRect("%").right;
+        return widthNumber + widthPercent;
+    }
+
+    private void computeTextPosition() {
+        String strNumber = Integer.toString(value);
+        String strPercent = "%";
+
+        paintText.setTextSize(sizeText);
+        Rect rectNumber = getStringRect(strNumber);
+        paintText.setTextSize(sizeText * factorPercent);
+        Rect rectPercent = getStringRect(strPercent);
+
+        float widthTotal = rectNumber.right + rectPercent.right;
+        float heightTotal = rectNumber.top;
+
+        float startxNumber = posCenter.x - widthTotal / 2;
+        float startyNumber = posCenter.y - heightTotal / 2;
+
+        float startxPercent = startxNumber + rectNumber.right;
+        float startyPercent = startyNumber + heightTotal - rectPercent.top;
+
+        posText_Value = new PointF(startxNumber, startyNumber);
+        posText_Percent = new PointF(startxPercent, startyPercent);
+    }
+
+    private void drawValueText(Canvas canvas) {
+        String strNumber = Integer.toString(value);
+        String strPercent = "%";
+
+        paintText.setTextSize(sizeText);
+        canvas.drawText(strNumber, posText_Value.x, posText_Value.y, paintText);
+        paintText.setTextSize(sizeText * factorPercent);
+        canvas.drawText(strPercent, posText_Percent.x, posText_Percent.y, paintText);
+    }
+
+    private void drawProgressCircle(Canvas canvas) {
+        canvas.rotate(-90, posCenter.x, posCenter.y);
+        canvas.drawCircle(posCenter.x, posCenter.y, radiusOuter, paintOuterCircle);
+        canvas.drawCircle(posCenter.x, posCenter.y, radiusInner, paintInnerCircle);
+        canvas.drawArc(rectOuterCircle_extended, 0, -3.6f * (100.0f - value), true, paintInnerCircle);
+        canvas.rotate(90, posCenter.x, posCenter.y);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        drawProgressCircle(canvas);
+        drawValueText(canvas);
+    }
+}
