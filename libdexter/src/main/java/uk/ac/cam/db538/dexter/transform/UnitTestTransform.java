42c3bc2055438df72d9230719748e4576cfb26c6
David Brazdil <david.brazdil@gmail.com>
2013-07-24 00:14:49 +0100

    UnitTestTransform assigns taint to 0xDEC0DED

--- libdexter/src/main/java/uk/ac/cam/db538/dexter/transform/UnitTestTransform.java
+++ libdexter/src/main/java/uk/ac/cam/db538/dexter/transform/UnitTestTransform.java
@@ -1,6 +1,10 @@
 package uk.ac.cam.db538.dexter.transform;
 
 import uk.ac.cam.db538.dexter.ProgressCallback;
+import uk.ac.cam.db538.dexter.dex.code.DexCode;
+import uk.ac.cam.db538.dexter.dex.code.elem.DexCodeElement;
+import uk.ac.cam.db538.dexter.dex.code.insn.DexInstruction_Const;
+import uk.ac.cam.db538.dexter.dex.code.macro.DexMacro;
 import uk.ac.cam.db538.dexter.dex.method.DexMethod;
 
 public class UnitTestTransform extends DexterTransform {
@@ -24,7 +28,23 @@ public class UnitTestTransform extends DexterTransform {
 		
 		return super.doFirst(method);
 	}
+	
+	@Override
+	public DexCodeElement doFirst(DexCodeElement element, DexCode code) {
+		if (element instanceof DexInstruction_Const) {
+			DexInstruction_Const insnConst = (DexInstruction_Const) element;
+			if (insnConst.getValue() == 0xDEC0DEDL)
+				return new DexMacro(
+					new DexInstruction_Const(
+						insnConst.getRegTo().getTaintRegister(), 
+						1L, 
+						insnConst.getHierarchy()),
+					insnConst);
+		}
 		
+		return super.doFirst(element, code);
+	}
+
 	private static final String TAINTCHECK_CLASS = "Lcom/dextertest/tests/TaintChecker;";
 	private static final String TAINTCHECK_METHOD = "isTainted";
 	private static final String TAINTCHECK_PROTOTYPE = "(I)Z";
