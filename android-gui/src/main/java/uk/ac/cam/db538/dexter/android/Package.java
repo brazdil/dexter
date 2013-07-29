41cb97fef554342e1b5e533749a7dbfc27d56d54
David Brazdil <david.brazdil@gmail.com>
2013-07-29 13:29:44 +0100

    Forgotten file

---
+++ android-gui/src/main/java/uk/ac/cam/db538/dexter/android/Package.java
@@ -0,0 +1,46 @@
+package uk.ac.cam.db538.dexter.android;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Package {
+
+    private PackageInfo pkg;
+    private PackageManager pm;
+
+    public Package(PackageManager pm, PackageInfo pkg) {
+        this.pm = pm;
+        this.pkg = pkg;
+    }
+
+    public String getApplicationName() {
+        return pkg.applicationInfo.loadLabel(pm).toString();
+    }
+
+    public Drawable getApplicationIcon() {
+        return pkg.applicationInfo.loadIcon(pm);
+    }
+
+    public String getPackageName() {
+        return pkg.packageName;
+    }
+
+    public File getPackageFile() {
+        return new File(pkg.applicationInfo.sourceDir);
+    }
+
+    public String getVersion() {
+        return pkg.versionName;
+    }
+
+    public String getLastUpdated() {
+        DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT);
+        return df.format(new Date(pkg.lastUpdateTime));
+    }
+}
