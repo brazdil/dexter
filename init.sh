ad0ff4469ebea0dcf80a99c56d2a6fe83b145c80
David Brazdil <david.brazdil@gmail.com>
2013-10-22 14:42:53 +0100

    Init script downloads the correct version of smali

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ rm -rf "$SMALI_DIR" || exit
 echo "cloning \"$SMALI_REPO\"..."
 git clone -q "$SMALI_REPO" "$SMALI_DIR" || exit
 echo "checking out the correct version..."
-(cd "$SMALI_DIR" && git checkout -q -b 'v1.4.2') || exit
+(cd "$SMALI_DIR" && git checkout -q 'tags/v1.4.2') || exit
 echo "deleting smali's gradle config file..." 
 rm "$SMALI_DIR"/build.gradle || exit
 
